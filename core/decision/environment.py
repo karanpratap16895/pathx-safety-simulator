4a522f3fba1fe9d918967912dd76f8622f55be3e
karanpratap16895 <karanpratap16895@gmail.com>
2025-12-16 17:36:15 +0530

    Delete core/decision directory

--- core/decision/environment.py
+++
@@ -1,8 +0,0 @@
-import time
-class EnvironmentState:
-    def __init__(self, wind=10):
-        self.wind_speed = wind
-        self.last_update = time.time()
-    def get_confidence(self):
-        age = time.time() - self.last_update
-        return 0.95 if age < 30 else 0.5
